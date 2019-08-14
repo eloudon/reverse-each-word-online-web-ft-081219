f268645c036d189be97bbd49c6290f6538b0bca0
Eric Loudon <loudon.eric@gmail.com>
2019-08-14 08:58:44 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,12 @@
+#def reverse_each_word(sentence)
+  #array = []
+  #(sentence.split).each do |word|
+    #array << word.reverse 
+  #end
+  #array.join(" ")
+#end 
+
+
 def reverse_each_word(sentence)
   array =[ ]
   new_sentence = " "
